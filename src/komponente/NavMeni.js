cfa0eebd8d04842b2020f1a6e2a05bbbbc7cdf28
djuricicjelena <djuricicjelenaa@gmail.com>
2023-04-25 00:47:56 +0200

    PocetnaDigimoni stranica i Nav komponenta i React Router ruta

---
+++ src/komponente/NavMeni.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function NavMeni() {
+    return (
+        <div className="nav_meni">
+            <Link to={"/"} id="digimoni_nav">DIGIMONI</Link>
+        </div>
+    )
+}
+
+export default NavMeni;
---
+++ src/strane/PocetnaDigimoni.js
@@ -0,0 +1,11 @@
+import NavMeni from "../komponente/NavMeni";
+
+function PocetnaDigimoni() {
+    return (
+        <div className="pocetna_digimoni">
+            <NavMeni />
+        </div>
+    )
+}
+
+export default PocetnaDigimoni;
